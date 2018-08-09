861d58320c79f719807bc749a28e1a9b540db9b6
tomo <px37282@yahoo.co.jp>
2018-08-09 21:49:11 +0900

    datepickerが表示できるようになった チャートが表示できるようになった（ハリボテ）

--- Viewer/functions/Temperature/main.py
+++ Viewer/functions/Temperature/main.py
@@ -1,4 +1,12 @@
-import jinja2
+from jinja2  import Environment, FileSystemLoader
+from os import path
+import boto3
+
+# DB接続情報
+dynamoDB = boto3.resource('dynamodb')
+table = dynamoDB.Table('temperature')
+
+templatePath = Environment(loader=FileSystemLoader(path.join(path.dirname(__file__), 'templates'), encoding='utf8'))
 
 # lambdaのハンドラ
 def handle(event, context):
@@ -6,5 +14,16 @@ def handle(event, context):
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "text/html"},
-        "body": "Hello"
+        "body": createPage()
     }
+
+# ページ生成メソッド
+# 日付・時刻・気温
+def createPage():
+    index = templatePath.get_template('index.html', )
+    return index.render(title=u"気温グラフ")
+
+def getTemperatue(date):
+    # DBにアクセスし、その日の気温を取得する
+    # return temperatue
+    return 0
