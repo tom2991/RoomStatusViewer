4d0e117932dd4f9f4d68f92b6830f7d25d7f38a4
tomo <px37282@yahoo.co.jp>
2018-08-17 12:56:06 +0900

    湿度をグラフ表示できるようにした

--- Viewer/functions/Temperature/main.py
+++ Viewer/functions/Temperature/main.py
@@ -6,7 +6,6 @@ from datetime import datetime, timedelta, timezone
 
 # DB接続情報
 dynamoDB = boto3.resource('dynamodb')
-table = dynamoDB.Table('temperature')
 
 templatePath = Environment(loader=FileSystemLoader(path.join(path.dirname(__file__), 'templates'), encoding='utf8'))
 
@@ -42,22 +41,41 @@ def createPage(dispDate):
 
 def cerateRenderData(date):
     # DBにアクセスし、その日の気温を取得する
-    response = table.scan(
-        FilterExpression=boto3.dynamodb.conditions.Attr('datetime_key').begins_with(date)
-    )
+    table = dynamoDB.Table('temperature')
+    responseItems = getResponceItems(table,date)
     temperateureList = []
-    timeList = []
-    responseItems = response['Items']
-    responseItems = sorted(responseItems, key=lambda x:x['datetime_key'])
+    temperateureTimeList = []
     for data in responseItems:
         temperateureList.append(data['value'])
         dateTime = data['datetime_key'].split("_")
-        timeList.append(dateTime[1][:4])
+        temperateureTimeList.append(dateTime[1][:4])
+
+    # DBにアクセスし、その日の湿度を取得する
+    table = dynamoDB.Table('humidity')
+    responseItems = getResponceItems(table,date)
+    humidityList = []
+    humidityTimeList = []
+    for data in responseItems:
+        humidityList.append(data['value'])
+        dateTime = data['datetime_key'].split("_")
+        humidityTimeList.append(dateTime[1][:4])
+
+    # チャートデータ生成
     renderData = {}
-    renderData['time'] = timeList
+    renderData['temp_time'] = temperateureTimeList
     renderData['temperature'] = temperateureList
+    renderData['humid_time'] = humidityTimeList
+    renderData['humidity'] = humidityList
 
     return renderData
+
+def getResponceItems(table,date):
+    response = table.scan(
+        FilterExpression=boto3.dynamodb.conditions.Attr('datetime_key').begins_with(date)
+    )
+    responseItems = response['Items']
+    return sorted(responseItems, key=lambda x:x['datetime_key'])
+
 
 # 今日の日付を返す
 def getNowDate():
